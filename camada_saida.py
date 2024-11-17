baf1caf9b2bf2c192445298aaf3e16ae328ddc44
patricknb <patricknascbue@gmail.com>
2024-11-17 12:16:02 -0300

    first commit

---
+++ camada_saida.py
@@ -0,0 +1,12 @@
+import numpy as np
+
+
+class CamadaSaida:
+
+    def __init__(self) -> None:
+        pass
+        
+    def softmax(y):
+        """Compute softmax values for each sets of scores in x."""
+        e_y = np.exp(y - np.max(y))
+        return e_y / e_y.sum()
---
+++ neuronio.py
@@ -0,0 +1,27 @@
+import random
+
+
+class Neuronio:
+
+    def __init__(self) -> None:
+        self.__atributos = []
+        self.__pesos_sinapticos = []
+        self.__vies = random.randint(1, 100)
+        self.__v = 0
+        self.__y = 0
+
+    def set_atributos(self, atributos:list):
+        self.__atributos = atributos
+
+    def gerar_pesos_aleatorios(self, nro_atributos:int):
+        for n in range(nro_atributos):
+            self.__pesos_sinapticos.append((random.randint(1,50)/100))
+
+    def combinador_linear(self):
+        for i in range(len(self.__atributos)):
+            self.__v = self.__atributos[i] * self.__pesos_sinapticos[i]
+        self.__v = self.__v + self.__vies
+
+    def funcao_de_ativacao(self):
+        self.__y = self.__v * (self.__v > 0)
+    
